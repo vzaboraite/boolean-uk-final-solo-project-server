1b884fbc7e23db0c20d19eb32a5d99ca63c0eef8
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-11-30 16:13:28 +0000

    Setup express

---
+++ src/index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const { APPCENTER } = require("ci-info");
+
+const app = express();
+
+/* SETUP MIDDLEWARE */
+
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+/* SETUP ROUTES */
+
+app.get("*", (req, res) => {
+  res.json({ ok: true });
+});
+
+/* START SERVER */
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+  console.log(`\n 🚀 Server is running on ${process.env.API_URL}\n`);
+});
