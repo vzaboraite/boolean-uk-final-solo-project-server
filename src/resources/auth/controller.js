ea35ed9751ca8cb3101840196f4a9bcc7563f674
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-01 12:27:40 +0000

    Build `signup` controller for auth resource

--- src/resources/auth/controller.js
+++ src/resources/auth/controller.js
@@ -1,3 +1,24 @@
 const prisma = require("../../utils/db");
 
-module.exports = {};
+async function signup(req, res) {
+  const userToCreate = { ...req.body };
+
+  if (!userToCreate.username || !userToCreate.password) {
+    res.status(400).json({ error: "Missing information!" });
+  }
+
+  try {
+    const user = awaitprisma.user.create({
+      data: {
+        ...userToCreate,
+      },
+    });
+    res.status(201).json({ user });
+  } catch (error) {
+    console.error("[ERROR] /signup route: ", error);
+
+    res.status(500).json({ error });
+  }
+}
+
+module.exports = { signup };
--- src/resources/auth/router.js
+++ src/resources/auth/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { signup } = require("./controller");
 
 const router = express.Router();
+
+router.post("/signup", signup);
 
 module.exports = router;
