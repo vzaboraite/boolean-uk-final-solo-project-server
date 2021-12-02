87318ee2884fd95b461e58dae6e127a2cdd78ef6
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-02 23:23:16 +0000

    Build `joinGame` controller

--- src/resources/games/controller.js
+++ src/resources/games/controller.js
@@ -48,14 +48,63 @@ async function getOneGame(req, res) {
   }
 }
 
+async function joinGame(req, res) {
+  const targetId = parseInt(req.params.id);
+
+  console.log(req.body);
+  const userId = req.body.userId;
+
+  try {
+    const result = await prisma.game.update({
+      /* Update doesn't allow multiple where conditions. 
+      Had to create extra condition `gameStatusId` in game table.
+      Refecence: // https://github.com/prisma/prisma/discussions/4185
+      */
+      where: {
+        gameStatusId: {
+          id: targetId,
+          gameStatus: "waiting",
+        },
+      },
+      data: {
+        users: {
+          create: {
+            user: {
+              connect: {
+                id: userId,
+              },
+            },
+          },
+        },
+        gameStatus: "in-progress",
+      },
+      include: {
+        users: {
+          include: {
+            user: {
+              select: {
+                id: true,
+                username: true,
+              },
+            },
+          },
+        },
+      },
     });
 
-    res.status(200).json({ game });
+    const cleanResult = {
+      ...result,
+      users: result.users.map((user) => {
+        return user.user;
+      }),
+    };
+
+    res.status(200).json({ cleanResult });
   } catch (error) {
-    console.error("[ERROR] /signup route: ", error);
+    console.error(`[ERROR] /games/${targetId}/join route: `, error);
 
     res.status(500).json({ error });
   }
 }
 
-module.exports = { getAllGames, getOneGame };
+module.exports = { getAllGames, getOneGame, joinGame };
--- src/resources/games/router.js
+++ src/resources/games/router.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { getAllGames, getOneGame } = require("./controller");
+const { getAllGames, getOneGame, joinGame } = require("./controller");
 
 const router = express.Router();
 
 router.get("/", getAllGames);
 
 router.get("/:id", getOneGame);
+
+router.put("/:id/join", joinGame);
 
 module.exports = router;
