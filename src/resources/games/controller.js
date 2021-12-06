d34f3361232f411d28675549315142884d151323
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-06 11:29:33 +0000

    Revert unique constraint in `Game` model

--- src/resources/games/controller.js
+++ src/resources/games/controller.js
@@ -17,7 +17,7 @@ async function getAllGames(req, res) {
 }
 
 async function getOneGame(req, res) {
-  const targetId = parseInt(req.params.id);
+  const targetId = req.params.id;
   try {
     const game = await prisma.game.findFirst({
       where: {
@@ -53,17 +53,13 @@ async function getOneGame(req, res) {
 }
 
 async function joinGame(req, res) {
-  const targetId = parseInt(req.params.id);
+  const targetId = req.params.id;
   const userId = req.user.id;
 
   try {
     const result = await prisma.game.update({
-      /* Update doesn't allow multiple where conditions. 
-      Had to create extra condition `gameStatusId` in game table.
-      Refecence: // https://github.com/prisma/prisma/discussions/4185
-      */
       where: {
-        gameStatusId: {
+        gameStatusIdConnector: {
           id: targetId,
           gameStatus: "waiting",
         },
