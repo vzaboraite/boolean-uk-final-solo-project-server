d5ccda28f56a06a3ad81c9602cf5c037c9526deb
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-12-14 12:02:03 +0000

    Build `getMovesByGameId` controller for moves resource

--- src/resources/moves/router.js
+++ src/resources/moves/router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { getMovesByGameId } = require("./controller");
 
 const router = express.Router();
+
+router.get("/game/:id", getMovesByGameId);
 
 module.exports = router;
